9a27eaf32fb5f04a40b93f835b14c1fd724e1c30
jarryp <jarry.palacios@hotmail.com>
2019-07-02 14:19:54 -0600

    incorporando el uso de consumo de apis con formato en json

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var source = $("#template_persona").html();
+var source = $("#template_paisesAll").html();
 var template = Handlebars.compile(source);
 
 Handlebars.registerHelper("enumeracion",function(pval,operador,sval){
@@ -15,15 +15,28 @@ Handlebars.registerHelper("enumeracion",function(pval,operador,sval){
    return operaciones[operador];
 });
 
+$.ajax({
+    url:"https://restcountries-v1.p.rapidapi.com/all",
+    type:"GET",
+    datatype:"json",
+    headers:{'X-RapidAPI-Key':'9cf42aa0b3msha9ae1a71ac4d6d4p1b1d37jsn926848524200'}
+}).done(function(data){
+    //console.log(data);
+    var html = template({'paises':data});
+    $("#contenedor").html(html);
+}).fail(function(){
+    console.log("error");
+}).always(function(){
+    console.log("completo");
+});
+
 var personas =[
     {'nombre':'Jairo','apellido':'Palacios R','correo':'jarryj.palacios@gmail.com'},
     {'nombre':'Jarry','apellido':'Palacios Rivas','correo':'palacios@gmail.com'},
     {'nombre':'Javier Alfonso','apellido':'Palacios','correo':'japalacios@gmail.com'},
 ]
 
-var html = template({'persona':personas});
 
-$("#contenedor").html(html);
 
 
 
