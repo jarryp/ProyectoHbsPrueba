16ac92e7f67afb71aefe6605fdac7f1b4f4d069c
jarryp <jarry.palacios@hotmail.com>
2019-07-02 09:53:33 -0600

    Manejo de arreglo de objetos e iteraciones para mostrar incorporando a la plantilla

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 var source = $("#template_persona").html();
 var template = Handlebars.compile(source);
 
-var persona ={'nombre':'Alfonso',
-              'apellido':'Palacios',
-              'correo':'jarryj.palacios@gmail.com'}
+var personas =[
+    {'nombre':'Jairo','apellido':'Palacios R','correo':'jarryj.palacios@gmail.com'},
+    {'nombre':'Jarry','apellido':'Palacios Rivas','correo':'palacios@gmail.com'},
+    {'nombre':'Javier Alfonso','apellido':'Palacios','correo':'japalacios@gmail.com'},
+]
 
-var html = template(persona);
+var html = template({'persona':personas});
 
 $("#contenedor").html(html);
 
