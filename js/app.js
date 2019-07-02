c2a8c227ec9f680fa7c6bd39b03c260fd245fcd7
jarryp <jarry.palacios@hotmail.com>
2019-07-02 10:38:57 -0600

    uso de la variable index, incorporacion de helpers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 var source = $("#template_persona").html();
 var template = Handlebars.compile(source);
 
+Handlebars.registerHelper("enumeracion",function(pval,operador,sval){
+   pval = parseFloat(pval);
+   sval = parseFloat(sval);
+
+   var operaciones ={
+       '+': pval + sval,
+       '-': pval - sval,
+       '*': pval * sval,
+       '/': pval / sval
+   }
+
+   return operaciones[operador];
+});
+
 var personas =[
     {'nombre':'Jairo','apellido':'Palacios R','correo':'jarryj.palacios@gmail.com'},
     {'nombre':'Jarry','apellido':'Palacios Rivas','correo':'palacios@gmail.com'},
